daae9c05524114d8eb9fd7c194ec11e12eca7b14
Sean McNamara <me@seanmcn.com>
2020-04-19 18:47:04 +0100

    Latest posts widget done, moved to widgets using generic widgetBox component

--- src/components/widgets/githubRepos/githubRepos.js
+++ src/components/widgets/githubRepos/githubRepos.js
@@ -1,39 +1,29 @@
 import React from 'react'
 import StaticData from '../../../data/static.json'
 import './githubRepos.scss'
+import WidgetBox from '../widgetBox'
+import GithubRepoListItem from './githubRepoListItem'
 
 const GithubReposWidget = () => (
-  <div className="box">
-    <h1 className="subtitle">Github Repos</h1>
-    <div className="content">
+  <WidgetBox
+    title="Github Repos"
+    content={(
       <ul className="link-list githubRepos">
-        {StaticData.github_repos.map((data) => {
+        {StaticData.github_repos.map(data => {
           return (
-            <li key={data.id}>
-              <a
-                href={data.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label={data.description}
-                title={data.description}
-                key={data.title}
-              >
-                {data.title}
-              </a>
-              {data.icons.map((icon) => {
-                return (
-                  <i className={`fab ${icon}`} key={icon} />
-                )
-              })}
-              <p className='description'>
-                {data.description}
-              </p>
-            </li>
+            <GithubRepoListItem
+              key={data.id}
+              id={data.id}
+              link={data.link}
+              description={data.description}
+              title={data.title}
+              icons={data.icons}
+            />
           )
         })}
       </ul>
-    </div>
-  </div>
+    )}
+  />
 )
 
 export default GithubReposWidget
---
+++ src/components/widgets/latestPosts/latestPostListItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import '../../blog/post/post.scss'
+import PropTypes from 'prop-types'
+
+const LatestPostListItem = ({ id, slug, title }) => (
+  <li key={id}>
+    <Link className="has-text-primary" to={`/blog/${slug}/`}>
+      {title}
+    </Link>
+  </li>
+)
+
+LatestPostListItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
+export default LatestPostListItem
