283a4b516604e66ced349624e2592ce3b1051c43
Sean McNamara <me@seanmcn.com>
2020-04-19 19:43:34 +0100

    refactor: make latest posts widget self-contained with internal data sourcing

--- src/components/widgets/latestPosts/latestPosts.js
+++ src/components/widgets/latestPosts/latestPosts.js
@@ -1,32 +1,38 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 import LatestPostListItem from './latestPostListItem'
 import WidgetBox from '../widgetBox'
 
-export default class LatestPostsWidget extends React.Component {
-  render() {
-    const { posts } = this.props
-
-    return (
-      <WidgetBox
-        title="Latest Posts"
-        content={(
-          <ul>
-            {posts.map(({ node: post }) => (
-              <LatestPostListItem
-                key={post.id}
-                id={post.id}
-                slug={post.slug}
-                title={post.title}
-              />
-            ))}
-          </ul>
-        )}
-      />
-    )
-  }
+const LatestPostsWidget = () => {
+  const data = useStaticQuery(graphql`
+    query latestPostsQuery {
+      allWordpressPost(sort: { fields: date, order: DESC }, limit: 4, skip: 0) {
+        edges {
+          node {
+            ...PostListFields
+          }
+        }
+      }
+    }
+  `)
+  const { edges: posts } = data.allWordpressPost
+  return (
+    <WidgetBox
+      title="Latest Posts"
+      content={(
+        <ul className="link-list">
+          {posts.map(({ node: post }) => (
+            <LatestPostListItem
+              key={post.id}
+              id={post.id}
+              slug={post.slug}
+              title={post.title}
+            />
+          ))}
+        </ul>
+      )}
+    />
+  )
 }
 
-LatestPostsWidget.propTypes = {
-  posts: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
+export default LatestPostsWidget
