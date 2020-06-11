298cf6b77e1760831f86116cdcddbb8285cbd1ca
Sean McNamara <me@seanmcn.com>
2020-06-11 14:42:28 +0100

    Updates all packages to latest version, adds reCAPTCHA v3 to the contact page.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 import Layout from '../components/layout/layout'
 import Sidebar from '../components/layout/sidebar'
+
+const recaptchaKey = '6LfhyvsUAAAAAK0VwkKfGF8YfWeTkD2lCeiLAvJ4'
+const getFormUrl = 'https://getform.io/f/d25c4df8-c821-4e9b-8e2f-94e08526caa0'
 
 const ContactPage = () => (
   <Layout>
@@ -9,70 +13,70 @@ const ContactPage = () => (
         <div className="box">
           <h1 className="title">Contact Me</h1>
           <div>
-            <form
-              action="https://getform.io/f/d25c4df8-c821-4e9b-8e2f-94e08526caa0"
-              method="POST"
-            >
-              <div className="field">
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label htmlFor="name" className="label">
-                  Name
-                </label>
+            <GoogleReCaptchaProvider reCaptchaKey={recaptchaKey}>
+              <form action={getFormUrl} method="POST">
+                <div className="field">
+                  {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                  <label htmlFor="name" className="label">
+                    Name
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Your name..."
+                      id="name"
+                      name="name"
+                    />
+                  </div>
+                </div>
+
+                <div className="field">
+                  {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                  <label htmlFor="email" className="label">
+                    Email
+                  </label>
+                  <div className="control has-icons-left">
+                    <input
+                      className="input"
+                      type="email"
+                      placeholder="Your email..."
+                      id="email"
+                      name="email"
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-envelope" />
+                    </span>
+                  </div>
+                </div>
+
+                <div className="field">
+                  {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                  <label htmlFor="message" className="label">
+                    Message
+                  </label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      placeholder="Your message...."
+                      id="message"
+                      name="message"
+                    />
+                  </div>
+                </div>
+
                 <div className="control">
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Your name..."
-                    id="name"
-                    name="name"
-                  />
+                  <button className="button is-link" type="submit">
+                    Send
+                  </button>
                 </div>
-              </div>
-
-              <div className="field">
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label htmlFor="email" className="label">
-                  Email
-                </label>
-                <div className="control has-icons-left">
-                  <input
-                    className="input"
-                    type="email"
-                    placeholder="Your email..."
-                    id="email"
-                    name="email"
-                  />
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-envelope" />
-                  </span>
-                </div>
-              </div>
-
-              <div className="field">
-                <label htmlFor="message" className="label">
-                  Message
-                </label>
-                <div className="control">
-                  <textarea
-                    className="textarea"
-                    placeholder="Your message...."
-                    id="message"
-                    name="message"
-                  />
-                </div>
-              </div>
-
-              <div className="control">
-                <button className="button is-link" type="submit">
-                  Send
-                </button>
-              </div>
-            </form>
+              </form>
+            </GoogleReCaptchaProvider>
           </div>
         </div>
       </div>
       <div className="column is-one-quarter" id="postSidebarColumn">
-          <Sidebar />
+        <Sidebar />
       </div>
     </div>
   </Layout>
