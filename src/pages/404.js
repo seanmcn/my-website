a20fa3ee9bcf52e1415eac43db8a97770adfe868
Sean McNamara <me@seanmcn.com>
2020-08-09 20:24:54 +0100

    docs(page): update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Layout from '../components/layout/layout'
+import Sidebar from '../components/layout/sidebar';
+import NotFoundImage from '../assets/images/404.png'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="columns">
+      <div className="column is-three-quarters">
+        <div className="box">
+          <h1 className='title'>404 - NOT FOUND</h1>
+          <p><strong>You just hit a route that doesn&#39;t exist... the sadness.</strong></p>
+          <img src={NotFoundImage} alt="404" />
+        </div>
+      </div>
+      <div className="column is-one-quarter">
+        <Sidebar />
+      </div>
     </div>
   </Layout>
 )
