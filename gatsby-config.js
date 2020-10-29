0eb43a4352f1e9329715811947f075a6e2ddea1a
Sean McNamara <me@seanmcn.com>
2020-10-29 02:38:52 +0000

    build: increase max image width in gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,3 @@
-const emoji = require(`remark-emoji`)
-const { gatsbyPluginFeed } = require('./src/utils/rss')
-const algoliaQueries = require('./src/utils/algolia')
-
-const isDevelopment = process.env.NODE_ENV === 'development'
-
-// Setup `gatsby-source-filesystem` for blog gatsbySourceFileSystemBlogPosts here as for development
-// builds we don't want all the blog images having to generate etc.
-const gatsbySourceFileSystemBlogPosts = {
-  resolve: `gatsby-source-filesystem`,
-  options: {
-    path: `${__dirname}/content/blog`,
-    name: `blog`,
-  },
-}
-
-if (isDevelopment) {
-  gatsbySourceFileSystemBlogPosts.options.ignore = [
-    '**/2009/**',
-    '**/2010/**',
-    '**/2011/**',
-    '**/2012/**',
-    '**/2013/**',
-    '**/2014/**',
-    '**/2015/**',
-    '**/2016/**',
-    '**/2017/**',
-    '**/learn/**',
-  ]
-}
-
-const gatsbySourceFileSystemAssets = {
-  resolve: `gatsby-source-filesystem`,
-  options: {
-    path: `${__dirname}/content/assets`,
-    name: `assets`,
-  },
-}
-
 module.exports = {
   siteMetadata: {
     title: `Sean McNamara`,
@@ -46,8 +7,20 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-sass',
-    gatsbySourceFileSystemBlogPosts,
-    gatsbySourceFileSystemAssets,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
@@ -62,17 +35,12 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 850,
-              linkImagesToOriginal: true,
             },
           },
           {
-            resolve: `gatsby-remark-embedder`,
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              services: {
-                YouTube: {
-                  height: "350px",
-                }
-              },
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
           {
@@ -82,20 +50,61 @@ module.exports = {
           {
             resolve: `gatsby-remark-smartypants`,
           },
-          {
-            resolve: "gatsby-remark-external-links",
-            options: {
-              target: "_self",
-              rel: "nofollow"
-            }
-          }
         ],
-        remarkPlugins: [emoji]
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    gatsbyPluginFeed,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return { ...edge.node.frontmatter, description: edge.node.excerpt,
+                  data: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],}
+              })
+            },
+            query: `
+            {
+              allMdx(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+              ) {
+                edges {
+                  node {
+                    frontmatter {
+                      title
+                      date
+                      slug
+                    }
+                    html
+                  }
+                }
+              }
+            }
+            `,
+            output: '/rss.xml',
+            title: 'Seanmcn.com RSS feed',
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -106,16 +115,6 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `content/assets/pwa/icon.png`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
-        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-        queries: algoliaQueries,
-        chunkSize: 10000,
       },
     },
     `gatsby-plugin-offline`,
