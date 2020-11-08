874bafa8fa01bcf3964a1c74d169a7707c55f601
Sean McNamara <me@seanmcn.com>
2020-11-08 00:21:33 +0000

    feat: extend breadcrumb component for category and tag pages

--- src/components/blog/breadcrumbs/breadcrumbs.js
+++ src/components/blog/breadcrumbs/breadcrumbs.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby';
 import './breadcrumbs.scss';
 import { slugToTitle } from '../../../utils/blog';
 
-const Breadcrumbs = ({ category, title, slug }) => (
+const Breadcrumbs = ({ category, tag, title, slug }) => (
   <div>
     <nav
       className="breadcrumb is-small has-succeeds-separator"
@@ -13,18 +13,31 @@ const Breadcrumbs = ({ category, title, slug }) => (
         <li key="/blog/">
           <Link to="/blog">Blog</Link>
         </li>
+        {category &&  (
         <li key={`/blog/categories/${category}`}>
           <Link to={`/blog/categories/${category}`}>
             {slugToTitle(category)}
           </Link>
         </li>
-        <li key={`/blog/${slug}`} className="is-active">
-          <Link
-            to={`/blog/${slug}`}
-            aria-current="page"
-            dangerouslySetInnerHTML={{ __html: title }}
-          />
-        </li>
+        ) }
+        {tag &&  (
+          <li key={`/blog/tags/${tag}`}>
+            <Link to={`/blog/tags/${tag}`}>
+              <i className="fas fa-tag" />
+              &nbsp;
+              {slugToTitle(tag)}
+            </Link>
+          </li>
+        ) }
+        {title && (
+          <li key={`/blog/${slug}`} className="is-active">
+            <Link
+              to={`/blog/${slug}`}
+              aria-current="page"
+              dangerouslySetInnerHTML={{ __html: title }}
+            />
+          </li>
+        )}
       </ul>
     </nav>
   </div>
