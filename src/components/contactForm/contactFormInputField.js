029bbbf9b2328c0e59cc079bd080a04498353448
Sean McNamara <me@seanmcn.com>
2020-11-15 15:49:51 +0000

    Fix to contact form input field labels

--- src/components/contactForm/contactFormInputField.js
+++ src/components/contactForm/contactFormInputField.js
@@ -7,21 +7,21 @@ const ContactFormInputField  = props => {
     <div className="field">
       <label htmlFor={id} className="label">
         {displayName}
-      </label>
-      <div className={type === 'email' ? 'control has-icons-left' : 'control'}>
-        <input
-          className="input"
-          type={type}
-          placeholder={placeholder}
-          id={id}
-          name={name}
-        />
-        {type === 'email' && (
+        <div className={type === 'email' ? 'control has-icons-left' : 'control'}>
+          <input
+            className="input"
+            type={type}
+            placeholder={placeholder}
+            id={id}
+            name={name}
+          />
+          {type === 'email' && (
           <span className="icon is-small is-left">
             <i className="fas fa-envelope" />
           </span>
         )}
-      </div>
+        </div>
+      </label>
     </div>
   )
 }
