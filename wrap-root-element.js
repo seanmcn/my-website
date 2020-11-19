27bd7fe2b688b090205bd8aaac40a9ce4eb48f08
Sean McNamara <me@seanmcn.com>
2020-11-19 02:33:16 +0000

    style: fix ESLint warnings and add suppressions for Gatsby files

--- wrap-root-element.js
+++ wrap-root-element.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import { MDXProvider } from '@mdx-js/react'
-import { preToCodeBlock } from 'mdx-utils'
-import { Code } from './src/components/code'
+import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
+import { preToCodeBlock } from 'mdx-utils';
+import { Code } from './src/components/code';
 
 // components is its own object outside of render so that the references to
 // components are stable
 const components = {
-  pre: preProps => {
-    const props = preToCodeBlock(preProps)
+  pre: (preProps) => {
+    const props = preToCodeBlock(preProps);
     // if there's a codeString and some props, we passed the test
     if (props) {
-      return <Code {...props} />
-    } 
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      return <Code {...props} />;
+    }
     // it's possible to have a pre without a code in it
-    return <pre {...preProps} />
-    
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    return <pre {...preProps} />;
   },
-}
+};
+// eslint-disable-next-line import/prefer-default-export
 export const wrapRootElement = ({ element }) => (
   <MDXProvider components={components}>{element}</MDXProvider>
-)
+);
