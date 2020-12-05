496c93fad5e13c8650525449077977a8610c80da
Sean McNamara <me@seanmcn.com>
2020-12-05 13:08:51 +0000

    feat: add santa emoji to navbar logo rotation

--- src/components/navbar/logo/navbarLogo.js
+++ src/components/navbar/logo/navbarLogo.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'gatsby';
-// import emoji from '../../../assets/images/emojis/75/beer.png'
 import './navbarLogo.scss';
 
 const NavbarLogo = () => {
   // eslint-disable-next-line global-require
-  const map = ['beer.png', 'cheer.png', 'thumbs-up.png', 'wave.png'];
+  const map = ['beer.png', 'cheer.png', 'thumbs-up.png', 'santa.png'];
   const randomMapKey = Math.floor(Math.random() * 4);
   // eslint-disable-next-line global-require,import/no-dynamic-require
   const emoji = require(`../../../assets/images/emojis/75/${map[randomMapKey]}`);
 
   return (
     <Link to="/" className="navbar-item" aria-label="Homepage">
-      <img src={emoji} alt="Emoji of Sean (varies between waving, thumbs up, cheering and having a beer)" className="emjoiLogo" />
+      <img src={emoji} alt="Emoji of Sean (varies between dressed as santa, thumbs up, cheering and having a beer)" className="emjoiLogo" />
       <h1 className="logoText">Sean McNamara</h1>
     </Link>
   );
