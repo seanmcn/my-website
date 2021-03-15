2c6c012247157e8d6dc0ded4e55a0cc9104f9e94
Sean McNamara <me@seanmcn.com>
2021-03-15 18:40:05 +0000

    Update logo to beer emoji

--- src/components/navbar/logo/navbarLogo.js
+++ src/components/navbar/logo/navbarLogo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import './navbarLogo.scss';
+import logo from '../../../assets/images/emojis/75/beer.png';
 
 const NavbarLogo = () => (
   <Link to="/" className="navbar-item" aria-label="Homepage">
-    <img src="../../../assets/images/emojis/75/beer.png" alt="Emoji of Sean (varies between dressed as santa, thumbs up, cheering and having a beer)" className="emjoiLogo" />
+    <img src={logo} alt="Emoji of Sean (varies between dressed as santa, thumbs up, cheering and having a beer)" className="emjoiLogo" />
     <h1 className="logoText">Sean McNamara</h1>
   </Link>
 );
