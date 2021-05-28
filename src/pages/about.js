e8375714642edb443572a860a87ebf89c2ba4638
Sean McNamara <me@seanmcn.com>
2021-05-28 03:01:41 +0100

    docs(page): update about me page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 import Layout from '../components/layout/layout';
-import staticData from '../data/static.json';
 import Sidebar from '../components/sidebar/sidebar';
 
 export default class AboutPage extends React.Component {
@@ -21,8 +20,42 @@ export default class AboutPage extends React.Component {
               <h1 className="title">About Me</h1>
               <div
                 className="content"
-                dangerouslySetInnerHTML={{ __html: staticData.about_me }}
-              />
+              >
+                <p>
+                  My journey into the world of programming started at a young
+                  age, fuelled by wanting to understand how computers work.
+                  I taught myself how to make some basic websites using HTML
+                  and CSS, and then quickly moved on to learn PHP.
+                </p>
+                <p>
+                  I entered the world of professional development in Kilkenny,
+                  Ireland as a freelancer, working on creating WordPress plugins
+                  and themes, and setting up e-commerce platforms. While working
+                  for myself had its advantages, I wanted to experience working
+                  with teams on larger projects.
+                </p>
+                <p>
+                  I then decided to leave Ireland to move to Canada on a working
+                  holiday visa. While there I had the chance to work with some
+                  amazing people at both Motbot and U.B.C&apos;s Library I.T.
+                  services team. When my working holiday ended, I moved to
+                  London, England where I started working at Kobas, a company
+                  that creates software for the hospitality industry.
+                </p>
+                <p>
+                  I&apos;m an advocate for open-source software and I&apos;m
+                  always trying to open source what I&apos;m working on as much
+                  as possible. I try to keep all my personal projects open
+                  source, and I&apos;m aiming to do more in this area moving
+                  forward.
+                </p>
+                <p>
+                  I&apos;m comfortable with programming in a lot of programming
+                  languages, but my two favourites are Python for file
+                  parsing/web scraping/console commands and PHP for web
+                  projects. Front-end development-wise I&apos;m a fan of ReactJS.
+                </p>
+              </div>
             </div>
           </div>
           <div className="column is-one-quarter">
