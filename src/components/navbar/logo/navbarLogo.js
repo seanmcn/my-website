bf5c6ecfdf5f1955ccd3f1f7bba6abcdfea3d3cc
Sean McNamara <me@seanmcn.com>
2021-08-16 16:58:58 +0100

    feat: re-enable dynamic logo rotation

--- src/components/navbar/logo/navbarLogo.js
+++ src/components/navbar/logo/navbarLogo.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import './navbarLogo.scss';
-import logo from '../../../assets/images/emojis/75/beer.png';
+import logoBeer from '../../../assets/images/emojis/75/beer.png';
+import logoCheer from '../../../assets/images/emojis/75/cheer.png';
+import logoThumbsUp from '../../../assets/images/emojis/75/thumbs-up.png';
 
-const NavbarLogo = () => (
-  <Link to="/" className="navbar-item" aria-label="Homepage">
-    <img src={logo} alt="Emoji of Sean (varies between dressed as santa, thumbs up, cheering and having a beer)" className="emjoiLogo" />
-    <h1 className="logoText">Sean McNamara</h1>
-  </Link>
-);
+const NavbarLogo = () => {
+  const map = [logoBeer, logoCheer, logoThumbsUp];
+  const randomMapKey = Math.floor(Math.random() * 3);
+  const emoji = map[randomMapKey];
+  return (
+    <Link to="/" className="navbar-item" aria-label="Homepage">
+      <img src={emoji} alt="Emoji of Sean (varies between dressed as santa, thumbs up, cheering and having a beer)" className="emjoiLogo" />
+      <h1 className="logoText">Sean McNamara</h1>
+    </Link>
+  );
+};
 
 export default NavbarLogo;
