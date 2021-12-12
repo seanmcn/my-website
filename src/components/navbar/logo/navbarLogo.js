86520e986fbb9463ba004baf8afbb3c1ead97c3d
Sean McNamara <me@seanmcn.com>
2021-12-12 15:32:59 +0000

    feat: add festive December logo

--- src/components/navbar/logo/navbarLogo.js
+++ src/components/navbar/logo/navbarLogo.js
@@ -4,11 +4,18 @@ import './navbarLogo.scss';
 import logoBeer from '../../../assets/images/emojis/75/beer.png';
 import logoCheer from '../../../assets/images/emojis/75/cheer.png';
 import logoThumbsUp from '../../../assets/images/emojis/75/thumbs-up.png';
+import logoSanta from '../../../assets/images/emojis/75/santa.png';
 
 const NavbarLogo = () => {
   const map = [logoBeer, logoCheer, logoThumbsUp];
   const randomMapKey = Math.floor(Math.random() * 3);
-  const emoji = map[randomMapKey];
+  let emoji = map[randomMapKey];
+
+  // If it's December, just display the Santa emoji.
+  const d = new Date();
+  if (d.getMonth() === 11) {
+    emoji = logoSanta;
+  }
   return (
     <Link to="/" className="navbar-item" aria-label="Homepage">
       <img src={emoji} alt="Emoji of Sean (varies between dressed as santa, thumbs up, cheering and having a beer)" className="emjoiLogo" />
