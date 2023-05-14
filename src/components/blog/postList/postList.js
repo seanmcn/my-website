7f2c4c4a69f60973c34fd7d889938229a55a0dad
Sean McNamara <me@seanmcn.com>
2023-05-14 16:38:03 +0000

    Adds a new component `postCard`, which displays featured image. Adds support for featured images

--- src/components/blog/postList/postList.js
+++ src/components/blog/postList/postList.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {graphql} from 'gatsby';
-import Post from '../post/post';
 import './postList.scss';
 import PostCard from '../postCard/postCard';
 
 export default class postList extends React.Component {
   render() {
     const {posts} = this.props;
-
+    console.log(posts);
     return (
       <div className={'is-flex is-flex-direction-row is-flex-wrap-wrap'}>
-        {posts.map(({node: post}) => (
-          <PostCard
-            key={post.id}
-            id={post.id}
-            content={post.body}
-            slug={post.frontmatter.slug}
-            title={post.frontmatter.title}
-            date={post.frontmatter.date}
-            tags={post.frontmatter.tags}
-          />
-        ))}
+        {posts.map(({node: post}) => {
+          const coverImage = post.frontmatter.featured ?
+            post.frontmatter.featured :
+            null;
+
+          return (
+            <PostCard
+              key={post.id}
+              id={post.id}
+              slug={post.frontmatter.slug}
+              title={post.frontmatter.title}
+              date={post.frontmatter.date}
+              tags={post.frontmatter.tags}
+              cover={coverImage}
+            />
+          );
+        })}
       </div>
     );
   }
 }
-
 postList.propTypes = {
   posts: PropTypes.arrayOf(PropTypes.object),
 };
@@ -40,6 +44,15 @@ export const pageQuery = graphql`
       slug
       tags
       category
+      featured {
+        childImageSharp {
+           gatsbyImageData(
+            placeholder: BLURRED
+            width: 300
+            formats: [AUTO, WEBP, AVIF]
+          )
+        }
+      }
     }
     body
   }
